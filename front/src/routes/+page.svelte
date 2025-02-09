78a8b8eb6e77a3b23cf8efd1405d44349e330cb5
asebrech <alois.sebrechts@gmail.com>
2025-02-09 18:12:59 +0100

    shadcn
    fix for tailwind v4 : https://github.com/shadcn-ui/ui/discussions/2996

--- front/src/routes/+page.svelte
+++ front/src/routes/+page.svelte
@@ -1,2 +1,8 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<script lang="ts">
+	import { Input } from '$lib/components/ui/input/index.js';
+	import { Button } from '$lib/components/ui/button/index.js';
+</script>
+
+<Input type="email" placeholder="email" class="max-w-xs" />
+
+<Button>Button</Button>
