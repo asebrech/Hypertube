ead27e40828af3b0fd2ea794dc7772ad4fb0fe6d
LaBoiteDeCarton <daniel.mercadiel@gmail.com>
2025-02-13 14:44:02 +0100

    modif

--- front/src/routes/+page.svelte
+++ front/src/routes/+page.svelte
@@ -7,7 +7,7 @@
 </script>
 
 <div>
-	<!-- comment -->
+	<!-- commentazkjhekehazjkh -->
 	<!-- <Input type="email" placeholder="email" class="max-w-xs" /> -->
 	<Button onclick={toggleMode} size="icon">
 		<Sun
