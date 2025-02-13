9259923f06a8fa50e7d7d0084d27ce6c433eb341
LaBoiteDeCarton <daniel.mercadiel@gmail.com>
2025-02-13 14:20:45 +0100

    first commit test

--- front/src/routes/+layout.svelte
+++ front/src/routes/+layout.svelte
@@ -1,6 +1,8 @@
 <script lang="ts">
 	import '../app.css';
 	let { children } = $props();
+	import { ModeWatcher } from 'mode-watcher';
 </script>
 
-{@render children()}
+<ModeWatcher defaultMode={'dark'} />
+{@render children?.()}
