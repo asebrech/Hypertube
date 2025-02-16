e20044a4df8d896fa188136193b3e5eb3a9a1a91
asebrech <alois.sebrechts@gmail.com>
2025-02-16 16:08:36 +0100

    access token implementation

--- back/start/routes.ts
+++ back/start/routes.ts
@@ -8,9 +8,31 @@
 */
 
 import router from '@adonisjs/core/services/router'
+import { middleware } from './kernel.js'
+
+const AuthController = () => import('#controllers/auth_controller')
 
 router.get('/', async () => {
   return {
     hello: 'world',
   }
 })
+
+router
+  .group(() => {
+    router.post('register', [AuthController, 'register'])
+    router.post('login', [AuthController, 'login'])
+    router.post('logout', [AuthController, 'logout']).use(middleware.auth())
+  })
+  .prefix('user')
+
+router
+  .get('me', async ({ auth, response }) => {
+    try {
+      const user = auth.getUserOrFail()
+      return response.ok(user)
+    } catch (error) {
+      return response.unauthorized({ error: 'User not found' })
+    }
+  })
+  .use(middleware.auth())
