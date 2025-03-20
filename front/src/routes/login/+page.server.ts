ab86761b1a9a55cc7b52958ccf4d65c704694d2d
asebrech <alois.sebrechts@gmail.com>
2025-03-20 14:21:52 +0100

    svelte .env for back url

--- front/src/routes/login/+page.server.ts
+++ front/src/routes/login/+page.server.ts
@@ -1,5 +1,6 @@
 import { fail, redirect, type RequestEvent } from '@sveltejs/kit';
 import axios from 'axios';
+import { SECRET_BACK_URL } from '$env/static/private';
 
 export const load = async ({ locals }: { locals: App.Locals }) => {
   if (locals.user) {
@@ -23,7 +24,7 @@ const login = async ({ cookies, request }: RequestEvent) => {
 
   const config = {
     method: 'post',
-    url: `${process.env.BACK_URL}/user/login`,
+    url: `${SECRET_BACK_URL}/user/login`,
     headers: {
       'Content-Type': 'application/json'
     },
