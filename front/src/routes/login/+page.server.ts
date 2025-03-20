1e2fb56c6bcdf85a4305bf0b2c8d72cfd4080545
asebrech <alois.sebrechts@gmail.com>
2025-03-20 17:32:45 +0100

    shadcn login

--- front/src/routes/login/+page.server.ts
+++ front/src/routes/login/+page.server.ts
@@ -3,59 +3,59 @@ import axios from 'axios';
 import { SECRET_BACK_URL } from '$env/static/private';
 
 export const load = async ({ locals }: { locals: App.Locals }) => {
-  if (locals.user) {
-    redirect(302, '/');
-  }
+	if (locals.user) {
+		redirect(302, '/');
+	}
 };
 
 const login = async ({ cookies, request }: RequestEvent) => {
-  const data = await request.formData();
-  const email = data.get('email');
-  const password = data.get('password');
+	const data = await request.formData();
+	const email = data.get('email');
+	const password = data.get('password');
 
-  if (typeof email !== 'string' || typeof password !== 'string' || !email || !password) {
-    return fail(400, { invalid: true });
-  }
+	if (typeof email !== 'string' || typeof password !== 'string' || !email || !password) {
+		return fail(400, { invalid: true });
+	}
 
-  const payload = JSON.stringify({
-    email,
-    password
-  });
+	const payload = JSON.stringify({
+		email,
+		password
+	});
 
-  const config = {
-    method: 'post',
-    url: `${SECRET_BACK_URL}/user/login`,
-    headers: {
-      'Content-Type': 'application/json'
-    },
-    data: payload
-  };
+	const config = {
+		method: 'post',
+		url: `${SECRET_BACK_URL}/user/login`,
+		headers: {
+			'Content-Type': 'application/json'
+		},
+		data: payload
+	};
 
-  try {
-    const response = await axios.request(config);
-    const token = response.data.token.token;
-    cookies.set('session', token, {
-      path: '/',
-      httpOnly: true,
-      sameSite: 'strict',
-      secure: true,
-      maxAge: 60 * 60 * 24 * 30
-    });
-    redirect(302, '/');
-  } catch (error) {
-    if (axios.isAxiosError(error) && error.response) {
-      switch (error.response.status) {
-        case 400:
-          return fail(400, { credentials: true });
-        case 422:
-          return fail(400, { invalid: true });
-        default:
-          throw error;
-      }
-    } else {
-      throw error;
-    }
-  }
+	try {
+		const response = await axios.request(config);
+		const token = response.data.token.token;
+		cookies.set('session', token, {
+			path: '/',
+			httpOnly: true,
+			sameSite: 'strict',
+			secure: true,
+			maxAge: 60 * 60 * 24 * 30
+		});
+		redirect(302, '/');
+	} catch (error) {
+		if (axios.isAxiosError(error) && error.response) {
+			switch (error.response.status) {
+				case 400:
+					return fail(400, { credentials: true });
+				case 422:
+					return fail(400, { invalid: true });
+				default:
+					throw error;
+			}
+		} else {
+			throw error;
+		}
+	}
 };
 
 export const actions = { login };
--- front/src/routes/login/+page.svelte
+++ front/src/routes/login/+page.svelte
@@ -1,29 +1,38 @@
 <script lang="ts">
-	let { form } = $props();
-
+	import { Button } from '$lib/components/ui/button/index.js';
+	import * as Card from '$lib/components/ui/card/index.js';
+	import { Input } from '$lib/components/ui/input/index.js';
+	import { Label } from '$lib/components/ui/label/index.js';
 	import { enhance } from '$app/forms';
+	let { form } = $props();
 </script>
 
-<h1>Login</h1>
-
-<form action="?/login" method="POST" use:enhance>
-	<div>
-		<label for="email">Username</label>
-		<input id="email" name="email" type="text" required />
-	</div>
-
-	<div>
-		<label for="password">Password</label>
-		<input id="password" name="password" type="password" required />
-	</div>
-
-	{#if form?.invalid}
-		<p class="error">Username and password is required.</p>
-	{/if}
-
-	{#if form?.credentials}
-		<p class="error">You have entered the wrong credentials.</p>
-	{/if}
-
-	<button type="submit">Log in</button>
-</form>
+<Card.Root class="mx-auto max-w-sm">
+	<Card.Header>
+		<Card.Title class="text-2xl">Login</Card.Title>
+		<Card.Description>Enter your email below to login to your account</Card.Description>
+	</Card.Header>
+	<Card.Content>
+		<form action="?/login" method="POST" use:enhance>
+			<div class="grid gap-4">
+				<div class="grid gap-2">
+					<Label for="email">Email</Label>
+					<Input id="email" type="email" name="email" placeholder="m@example.com" required />
+				</div>
+				<div class="grid gap-2">
+					<div class="flex items-center">
+						<Label for="password">Password</Label>
+						<a href="##" class="ml-auto inline-block text-sm underline"> Forgot your password? </a>
+					</div>
+					<Input id="password" type="password" name="password" required />
+				</div>
+				<Button type="submit" class="w-full">Login</Button>
+				<Button variant="outline" class="w-full">Login with Google</Button>
+			</div>
+			<div class="mt-4 text-center text-sm">
+				Don&apos;t have an account?
+				<a href="##" class="underline">Sign up</a>
+			</div>
+		</form>
+	</Card.Content>
+</Card.Root>
