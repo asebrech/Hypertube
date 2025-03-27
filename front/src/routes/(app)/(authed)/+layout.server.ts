e01f36e502bf95c11d0afa76abbcfd8179ced4c1
asebrech <alois.sebrechts@gmail.com>
2025-03-27 17:37:27 +0100

    return to profile

--- front/src/routes/(app)/(authed)/+layout.server.ts
+++ front/src/routes/(app)/(authed)/+layout.server.ts
@@ -2,6 +2,6 @@ import { redirect } from '@sveltejs/kit';
 
 export const load = async ({ locals }: { locals: App.Locals }) => {
   if (!locals.user) {
-    redirect(302, '/');
+    redirect(302, '/login');
   }
 };
