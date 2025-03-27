2093f1c35753e4d1871f5e53f7d27498bcaae409
asebrech <alois.sebrechts@gmail.com>
2025-03-27 16:21:00 +0100

    update route system

---
+++ front/src/routes/(app)/+layout.svelte
@@ -0,0 +1,24 @@
+<script lang="ts">
+	let { children } = $props();
+	import { ModeWatcher } from 'mode-watcher';
+	import { page } from '$app/stores';
+
+	import { enhance } from '$app/forms';
+</script>
+
+<ModeWatcher defaultMode={'dark'} />
+
+<nav>
+	{#if !$page.data.user}
+		<a href="/login">Login</a>
+		<a href="/register">Register</a>
+	{/if}
+
+	{#if $page.data.user}
+		<form action="/logout" method="POST" use:enhance>
+			<button type="submit">Log out</button>
+		</form>
+	{/if}
+</nav>
+
+{@render children?.()}
--- front/src/routes/+layout.svelte
+++ front/src/routes/+layout.svelte
@@ -1,29 +1,6 @@
 <script lang="ts">
 	import '../app.css';
 	let { children } = $props();
-	import { ModeWatcher } from 'mode-watcher';
-	import { page } from '$app/stores';
-
-	import { enhance } from '$app/forms';
 </script>
-
-<ModeWatcher defaultMode={'dark'} />
-
-<svelte:head>
-	<title>SvelteKit Auth</title>
-</svelte:head>
-
-<nav>
-	{#if !$page.data.user}
-		<a href="/login">Login</a>
-		<a href="/register">Register</a>
-	{/if}
-
-	{#if $page.data.user}
-		<form action="/logout" method="POST" use:enhance>
-			<button type="submit">Log out</button>
-		</form>
-	{/if}
-</nav>
 
 {@render children?.()}
