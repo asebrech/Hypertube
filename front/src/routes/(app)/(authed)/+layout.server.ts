7b37c7296d9c59bc57882a33ef97cca5addefaf8
asebrech <alois.sebrechts@gmail.com>
2025-03-28 11:11:07 +0100

    carousel preview

--- front/src/routes/(app)/(authed)/+layout.server.ts
+++ front/src/routes/(app)/(authed)/+layout.server.ts
@@ -1,7 +1,7 @@
 import { redirect } from '@sveltejs/kit';
 
 export const load = async ({ locals }: { locals: App.Locals }) => {
-  if (!locals.user) {
-    redirect(302, '/login');
-  }
+	if (!locals.user) {
+		redirect(302, '/login');
+	}
 };
