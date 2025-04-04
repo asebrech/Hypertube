6fef7f69c514befe4816aad4a893c93817d99e2b
asebrech <alois.sebrechts@gmail.com>
2025-04-04 09:34:09 +0200

    reconciliation with email

--- front/src/routes/(auth)/(authed)/login/+page.svelte
+++ front/src/routes/(auth)/(authed)/login/+page.svelte
@@ -14,25 +14,32 @@
 	</Card.Header>
 	<Card.Content>
 		<div class="grid gap-4">
-			<div class="grid gap-2">
-				{#if form?.credentials}
-					<p class="error">You have entered the wrong credentials.</p>
-				{/if}
-				<Label for="email">Email</Label>
-				<Input id="email" type="email" name="email" placeholder="m@example.com" required />
-			</div>
 			<form action="?/login" method="POST" use:enhance>
-				<div class="grid gap-2">
-					<div class="flex items-center">
-						<Label for="password">Password</Label>
-						<a href="##" class="ml-auto inline-block text-sm underline"> Forgot your password? </a>
+				<div class="grid gap-4">
+					<div class="grid gap-2">
+						{#if form?.credentials}
+							<p class="error">You have entered the wrong credentials.</p>
+						{/if}
+						<Label for="email">Email</Label>
+						<Input id="email" type="email" name="email" placeholder="m@example.com" required />
 					</div>
-					<Input id="password" type="password" name="password" required />
+					<div class="grid gap-2">
+						<div class="flex items-center">
+							<Label for="password">Password</Label>
+							<a href="##" class="ml-auto inline-block text-sm underline">
+								Forgot your password?
+							</a>
+						</div>
+						<Input id="password" type="password" name="password" required />
+					</div>
+					<Button type="submit" class="w-full">Login</Button>
 				</div>
-				<Button type="submit" class="w-full">Login</Button>
+			</form>
+			<form method="POST" action="?/google" use:enhance>
+				<Button variant="outline" class="w-full" type="submit">Login with Google</Button>
 			</form>
 			<form method="POST" action="?/github" use:enhance>
-				<Button variant="outline" class="w-full" type="submit">Login with Google</Button>
+				<Button variant="outline" class="w-full" type="submit">Login with Gihub</Button>
 			</form>
 		</div>
 		<div class="mt-4 text-center text-sm">
