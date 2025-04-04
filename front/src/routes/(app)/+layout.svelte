38e5bc4b05faab1958b96cd3f1ff3d56364e9388
LaBoiteDeCarton <daniel.mercadiel@gmail.com>
2025-04-04 10:38:38 +0200

    langage selector

--- front/src/routes/(app)/+layout.svelte
+++ front/src/routes/(app)/+layout.svelte
@@ -7,6 +7,9 @@
 
 	import * as Card from '$lib/components/ui/card/index.js';
 	import { Button } from '$lib/components/ui/button/index.js';
+	import * as Select from '$lib/components/ui/select/index.js';
+	import { locale, locales } from 'svelte-i18n';
+	import { _ } from 'svelte-i18n';
 </script>
 
 <Card.Root class="mb-5">
@@ -14,16 +17,30 @@
 		<a href="/">
 			<Icon />
 		</a>
-		<div>
+		<div class="flex items-center gap-4">
+			<Select.Root bind:value={$locale} type="single">
+				<Select.Trigger class="flex gap-2"
+					><img src="/icons/langage.svg" class="w-[13px]" />{$locale == 'fr-FR'
+						? 'Français'
+						: 'English'}</Select.Trigger
+				>
+				<Select.Content>
+					{#each $locales as locale_item}
+						<Select.Item value={locale_item}
+							>{locale_item == 'fr-FR' ? 'Français' : 'English'}</Select.Item
+						>
+					{/each}
+				</Select.Content>
+			</Select.Root>
 			{#if !$page.data.user}
 				<a href="/login">
-					<Button>Sign in</Button>
+					<Button>{$_('sign_in')}</Button>
 				</a>
 			{/if}
 
 			{#if $page.data.user}
 				<form action="/logout" method="POST" use:enhance>
-					<Button type="submit">Log out</Button>
+					<Button type="submit">{$_('log_out')}</Button>
 				</form>
 			{/if}
 		</div>
