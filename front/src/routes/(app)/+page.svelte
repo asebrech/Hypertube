ca8e7bdcc60ca033d7cc9261833117db2e4d44dc
LaBoiteDeCarton <daniel.mercadiel@gmail.com>
2025-05-22 15:25:20 +0200

    y ena marre

--- front/src/routes/(app)/+page.svelte
+++ front/src/routes/(app)/+page.svelte
@@ -1,12 +1,5 @@
 <script lang="ts">
-	import {
-		Card,
-		CardContent,
-		CardDescription,
-		CardFooter,
-		CardHeader,
-		CardTitle
-	} from '@/components/ui/card';
+	import { Card, CardHeader, CardTitle } from '@/components/ui/card';
 	import {
 		Carousel,
 		CarouselContent,
@@ -19,6 +12,8 @@
 	import { PUBLIC_BACK_URL } from '$env/static/public';
 	import { locale } from 'svelte-i18n';
 	import { Skeleton } from '@/components/ui/skeleton';
+	import { HoverCard, HoverCardTrigger, HoverCardContent } from '@/components/ui/hover-card';
+	import { Badge } from '$lib/components/ui/badge/index.js';
 
 	type BackDropImage = {
 		url: string;
@@ -38,10 +33,62 @@
 			backdrop_image: BackDropImage | null | undefined;
 		}[];
 	};
+
+	type MovieDetails = {
+		adult: boolean;
+		backdrop_path: string | null;
+		belongs_to_collection?: {
+			id: number;
+			name: string;
+			poster_path: string | null;
+			backdrop_path: string | null;
+		};
+		budget: number;
+		genres: {
+			id: number;
+			name: string;
+		}[];
+		homepage: string | null;
+		id: number;
+		imdb_id: string | null;
+		origin_country: string[];
+		original_language: string;
+		original_title: string;
+		overview: string;
+		popularity: number;
+		poster_path: string | null;
+		production_companies: {
+			id: number;
+			logo_path: string | null;
+			name: string;
+			origin_country: string;
+		}[];
+		production_countries: {
+			iso_3166_1: string;
+			name: string;
+		}[];
+		release_date: string;
+		revenue: number;
+		runtime: number | null;
+		spoken_languages: {
+			english_name: string;
+			iso_639_1: string;
+			name: string;
+		}[];
+		status: string;
+		tagline: string;
+		title: string;
+		video: boolean;
+		vote_average: number;
+		vote_count: number;
+	};
+
 	export let hasMorePages: boolean;
 	export let currentPage: number = 1;
 	export let isLoading: boolean;
+	export let isLoadingDetails: boolean;
 	export let movies_genres: MovieGenre[] = [];
+	export let movieDetails: MovieDetails[];
 
 	const getMovies = async (page_to_load: number) => {
 		const config = {
@@ -58,6 +105,23 @@
 			return response.data;
 		} catch (error) {
 			console.error('Error fetching movies:', error);
+			throw error;
+		}
+	};
+
+	const getMovieDetails = async (movieId: number) => {
+		const config = {
+			method: 'get',
+			url: `${PUBLIC_BACK_URL}/movies/${movieId}`,
+			params: {
+				lang: $locale
+			}
+		};
+		try {
+			const response = await axios(config);
+			return response.data;
+		} catch (error) {
+			console.error('Error fetching movie details:', error);
 			throw error;
 		}
 	};
@@ -109,6 +173,11 @@
 		}
 	};
 
+	const loadMovieDetails = async (movieId: number): Promise<MovieDetails> => {
+		const movieDetailsResponse = await getMovieDetails(movieId);
+		return movieDetailsResponse;
+	};
+
 	onMount(async () => {
 		if (currentPage === 1) {
 			isLoading = true;
@@ -149,45 +218,99 @@
 			opts={{
 				align: 'start',
 				loop: true,
-				slidesToScroll: 5
+				slidesToScroll: 2,
+				startIndex: 0,
+				breakpoints: {
+					// Tailwind's breakpoints in pixels
+					'(min-width: 640px)': { slidesToScroll: 3 }, // sm
+					'(min-width: 768px)': { slidesToScroll: 4 }, // md
+					'(min-width: 1024px)': { slidesToScroll: 5 }, // lg
+					'(min-width: 1280px)': { slidesToScroll: 6 } // xl
+				}
 			}}
 			class="w-[calc(100vw + 160px)] ml-[-160px]"
 		>
 			<CarouselContent class="ml-0 flex gap-[6px]">
 				{#each genre.movies as movie}
 					<CarouselItem class="basis-auto p-0">
-						{#if movie.backdrop_image}
-							<Card
-								class="jystify-end flex h-[123px] w-[218px] flex-row rounded-[2px] border-none p-0"
-								style="background-size: cover; background-position: center; background-image: url({movie
-									.backdrop_image.url});"
-							>
-								{#if !movie.backdrop_image.langFound}
-									<CardHeader class="bg-black bg-opacity-50 p-4">
-										<CardTitle>{movie.title}</CardTitle>
-									</CardHeader>
+						<HoverCard openDelay={100} closeDelay={100}>
+							<HoverCardTrigger href="/movie/{movie.id}" rel="noreferrer noopener">
+								{#if movie.backdrop_image}
+									<Card
+										class="jystify-end flex h-[123px] w-[218px] flex-row rounded-[2px] border-none p-0"
+										style="background-size: cover; background-position: center; background-image: url({movie
+											.backdrop_image.url});"
+									>
+										{#if !movie.backdrop_image.langFound}
+											<CardHeader class="bg-black bg-opacity-50 p-4">
+												<CardTitle>{movie.title}</CardTitle>
+											</CardHeader>
+										{/if}
+									</Card>
+								{:else}
+									{#await loadBackdropImage(movie.id, 'small')}
+										<Card class="flex h-[123px] w-[218px] flex-row rounded-[2px] border-none p-0">
+											<div class="h-full w-full">
+												<Skeleton class="h-full w-full rounded-[2px]" />
+											</div>
+										</Card>
+									{:then updatedBackdropImage}
+										<Card
+											class="jystify-end flex h-[123px] w-[218px] flex-row rounded-[2px] border-none p-0"
+											style="background-size: cover; background-position: center; background-image: url({updatedBackdropImage?.url});"
+										>
+											{#if !updatedBackdropImage?.langFound}
+												<CardHeader class="bg-black bg-opacity-50 p-4">
+													<CardTitle>{movie.title}</CardTitle>
+												</CardHeader>
+											{/if}
+										</Card>
+									{/await}
 								{/if}
-							</Card>
-						{:else}
-							{#await loadBackdropImage(movie.id, 'small')}
-								<Card class="flex h-[123px] w-[218px] flex-row rounded-[2px] border-none p-0">
-									<div class="h-full w-full">
-										<Skeleton class="h-full w-full rounded-[2px]" />
+							</HoverCardTrigger>
+							<HoverCardContent
+								hideWhenDetached={true}
+								avoidCollisions={false}
+								side="bottom"
+								sideOffset={-231}
+								class="mt-0 w-[300px] overflow-hidden rounded-[8px] border-none p-0"
+							>
+								{#await loadMovieDetails(movie.id)}
+									<Skeleton class="h-full w-full" />
+								{:then movieDetails}
+									<div class="flex flex-col items-center">
+										{#if movieDetails.backdrop_path}
+											<img
+												src={`https://image.tmdb.org/t/p/w500${movieDetails.backdrop_path}`}
+												alt={movieDetails.title}
+												class="w-[300px] rounded-[2px] object-cover"
+											/>
+										{/if}
+										<div class="m-2 p-4">
+											<h3 class="line-clamp-1 font-medium">{movieDetails.title}</h3>
+											<p class="line-clamp-3 text-sm text-gray-500">{movieDetails.overview}</p>
+											{#if movieDetails.runtime}
+												<p class="text-sm text-gray-500">
+													{Math.floor(movieDetails.runtime / 60)}h {movieDetails.runtime % 60}m
+												</p>
+											{/if}
+											{#if movieDetails.genres.length > 0}
+												<div class="flex flex-wrap justify-center">
+													{#each movieDetails.genres as genre}
+														<Badge
+															class="m-1 rounded-[2px] bg-gray-200 px-2 py-1 text-sm text-gray-700"
+															key={genre.id}
+														>
+															{genre.name}
+														</Badge>
+													{/each}
+												</div>
+											{/if}
+										</div>
 									</div>
-								</Card>
-							{:then updatedBackdropImage}
-								<Card
-									class="jystify-end flex h-[123px] w-[218px] flex-row rounded-[2px] border-none p-0"
-									style="background-size: cover; background-position: center; background-image: url({updatedBackdropImage?.url});"
-								>
-									{#if !updatedBackdropImage?.langFound}
-										<CardHeader class="bg-black bg-opacity-50 p-4">
-											<CardTitle>{movie.title}</CardTitle>
-										</CardHeader>
-									{/if}
-								</Card>
-							{/await}
-						{/if}
+								{/await}
+							</HoverCardContent>
+						</HoverCard>
 					</CarouselItem>
 				{/each}
 			</CarouselContent>
