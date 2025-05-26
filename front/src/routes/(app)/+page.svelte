cb72e68e9782aed78697399ad27857bc8d249bc4
LaBoiteDeCarton <daniel.mercadiel@gmail.com>
2025-05-26 13:54:14 +0200

    adding horizontal fetching

--- front/src/routes/(app)/+page.svelte
+++ front/src/routes/(app)/+page.svelte
@@ -14,6 +14,28 @@
 	import { Skeleton } from '@/components/ui/skeleton';
 	import { HoverCard, HoverCardTrigger, HoverCardContent } from '@/components/ui/hover-card';
 	import { Badge } from '$lib/components/ui/badge/index.js';
+	import type { Action } from 'svelte/action';
+	import { writable } from 'svelte/store';
+
+	export const visibleSlides = writable<number[]>([]);
+
+	export const inView: Action<HTMLElement, (visible: boolean) => void> = (node, callback) => {
+		const observer = new IntersectionObserver(([entry]) => {
+			callback(entry.isIntersecting);
+		});
+
+		observer.observe(node);
+
+		return {
+			destroy() {
+				observer.unobserve(node);
+			}
+		};
+	};
+
+	function handleVisibility(index: number, visible: boolean) {
+		if (visible) visibleSlides.update((visibleSlides) => [...visibleSlides, index]);
+	}
 
 	type BackDropImage = {
 		url: string;
@@ -101,7 +123,6 @@
 		};
 		try {
 			const response = await axios(config);
-			console.log('response', response.data);
 			return response.data;
 		} catch (error) {
 			console.error('Error fetching movies:', error);
@@ -231,88 +252,105 @@
 			class="w-[calc(100vw + 160px)] ml-[-160px]"
 		>
 			<CarouselContent class="ml-0 flex gap-[6px]">
-				{#each genre.movies as movie}
+				{#each genre.movies as movie, index}
 					<CarouselItem class="basis-auto p-0">
-						<HoverCard openDelay={100} closeDelay={100}>
-							<HoverCardTrigger href="/movie/{movie.id}" target="_blank" rel="noreferrer noopener">
-								{#if movie.backdrop_image}
-									<Card
-										class="jystify-end flex h-[123px] w-[218px] flex-row rounded-[2px] border-none p-0"
-										style="background-size: cover; background-position: center; background-image: url({movie
-											.backdrop_image.url});"
-									>
-										{#if !movie.backdrop_image.langFound}
-											<CardHeader class="bg-black bg-opacity-50 p-4">
-												<CardTitle>{movie.title}</CardTitle>
-											</CardHeader>
-										{/if}
-									</Card>
-								{:else}
-									{#await loadBackdropImage(movie.id, 'small')}
-										<Card class="flex h-[123px] w-[218px] flex-row rounded-[2px] border-none p-0">
-											<div class="h-full w-full">
-												<Skeleton class="h-full w-full rounded-[2px]" />
-											</div>
-										</Card>
-									{:then updatedBackdropImage}
+						<div use:inView={(visible) => handleVisibility(index, visible)}>
+							<HoverCard openDelay={100} closeDelay={100}>
+								<HoverCardTrigger
+									href="/movie/{movie.id}"
+									target="_blank"
+									rel="noreferrer noopener"
+								>
+									{#if movie.backdrop_image}
 										<Card
 											class="jystify-end flex h-[123px] w-[218px] flex-row rounded-[2px] border-none p-0"
-											style="background-size: cover; background-position: center; background-image: url({updatedBackdropImage?.url});"
+											style="background-size: cover; background-position: center; background-image: url({movie
+												.backdrop_image.url});"
 										>
-											{#if !updatedBackdropImage?.langFound}
+											{#if !movie.backdrop_image.langFound}
 												<CardHeader class="bg-black bg-opacity-50 p-4">
 													<CardTitle>{movie.title}</CardTitle>
 												</CardHeader>
 											{/if}
 										</Card>
-									{/await}
-								{/if}
-							</HoverCardTrigger>
-							<HoverCardContent
-								hideWhenDetached={true}
-								avoidCollisions={false}
-								side="bottom"
-								sideOffset={-231}
-								class="mt-0 w-[300px] overflow-hidden rounded-[8px] border-none p-0"
-							>
-								<a href="/movie/{movie.id}" target="_blank" rel="noreferrer noopener" class="block">
-									{#await loadMovieDetails(movie.id)}
-										<Skeleton class="h-full w-full" />
-									{:then movieDetails}
-										<div class="flex flex-col items-center">
-											{#if movieDetails.backdrop_path}
-												<img
-													src={`https://image.tmdb.org/t/p/w500${movieDetails.backdrop_path}`}
-													alt={movieDetails.title}
-													class="w-[300px] rounded-[2px] object-cover"
-												/>
-											{/if}
-											<div class="m-2 p-4">
-												<h3 class="line-clamp-1 font-medium">{movieDetails.title}</h3>
-												<p class="line-clamp-3 text-sm text-gray-500">{movieDetails.overview}</p>
-												{#if movieDetails.runtime}
-													<p class="text-sm text-gray-500">
-														{Math.floor(movieDetails.runtime / 60)}h {movieDetails.runtime % 60}m
-													</p>
+									{:else if $visibleSlides.includes(index)}
+										{#await loadBackdropImage(movie.id, 'small')}
+											<Card class="flex h-[123px] w-[218px] flex-row rounded-[2px] border-none p-0">
+												<div class="h-full w-full">
+													<Skeleton class="h-full w-full rounded-[2px]" />
+												</div>
+											</Card>
+										{:then updatedBackdropImage}
+											<Card
+												class="jystify-end flex h-[123px] w-[218px] flex-row rounded-[2px] border-none p-0"
+												style="background-size: cover; background-position: center; background-image: url({updatedBackdropImage?.url});"
+											>
+												{#if !updatedBackdropImage?.langFound}
+													<CardHeader class="bg-black bg-opacity-50 p-4">
+														<CardTitle>{movie.title}</CardTitle>
+													</CardHeader>
 												{/if}
-												{#if movieDetails.genres.length > 0}
-													<div class="flex flex-wrap justify-center">
-														{#each movieDetails.genres as genre}
-															<Badge
-																class="m-1 rounded-[2px] bg-gray-200 px-2 py-1 text-sm text-gray-700"
-																key={genre.id}
-															>
-																{genre.name}
-															</Badge>
-														{/each}
-													</div>
-												{/if}
+											</Card>
+										{/await}
+									{:else}
+										<Card class="flex h-[123px] w-[218px] flex-row rounded-[2px] border-none p-0">
+											<div class="h-full w-full">
+												<Skeleton class="h-full w-full rounded-[2px]" />
 											</div>
-										</div>
-									{/await}
-								</a>
-							</HoverCardContent>
-						</HoverCard>
+										</Card>
+									{/if}
+								</HoverCardTrigger>
+								<HoverCardContent
+									hideWhenDetached={true}
+									avoidCollisions={false}
+									side="bottom"
+									sideOffset={-231}
+									class="mt-0 w-[300px] overflow-hidden rounded-[8px] border-none p-0"
+								>
+									<a
+										href="/movie/{movie.id}"
+										target="_blank"
+										rel="noreferrer noopener"
+										class="block"
+									>
+										{#await loadMovieDetails(movie.id)}
+											<Skeleton class="h-full w-full" />
+										{:then movieDetails}
+											<div class="flex flex-col items-center">
+												{#if movieDetails.backdrop_path}
+													<img
+														src={`https://image.tmdb.org/t/p/w500${movieDetails.backdrop_path}`}
+														alt={movieDetails.title}
+														class="w-[300px] rounded-[2px] object-cover"
+													/>
+												{/if}
+												<div class="m-2 p-4">
+													<h3 class="line-clamp-1 font-medium">{movieDetails.title}</h3>
+													<p class="line-clamp-3 text-sm text-gray-500">{movieDetails.overview}</p>
+													{#if movieDetails.runtime}
+														<p class="text-sm text-gray-500">
+															{Math.floor(movieDetails.runtime / 60)}h {movieDetails.runtime % 60}m
+														</p>
+													{/if}
+													{#if movieDetails.genres.length > 0}
+														<div class="flex flex-wrap justify-center">
+															{#each movieDetails.genres as genre}
+																<Badge
+																	class="m-1 rounded-[2px] bg-gray-200 px-2 py-1 text-sm text-gray-700"
+																	key={genre.id}
+																>
+																	{genre.name}
+																</Badge>
+															{/each}
+														</div>
+													{/if}
+												</div>
+											</div>
+										{/await}
+									</a>
+								</HoverCardContent>
+							</HoverCard>
+						</div>
 					</CarouselItem>
 				{/each}
 			</CarouselContent>
