e3289c8d0d8d5876c9cf10d7c1ddbe696d3cb8c1
asebrech <alois.sebrechts@gmail.com>
2025-06-05 17:48:54 +0200

    merge turborepo

--- front/src/routes/(app)/(authed)/+layout.server.ts
+++
@@ -1,7 +0,0 @@
-import { redirect } from '@sveltejs/kit';
-
-export const load = async ({ locals }: { locals: App.Locals }) => {
-	if (!locals.user) {
-		redirect(302, '/login');
-	}
-};
--- front/src/routes/(auth)/(authed)/login/[provider]/+page.server.ts
+++
@@ -1,9 +0,0 @@
-import { SECRET_BACK_REDIRECT_URL } from '$env/static/private';
-import { redirect, type RequestEvent } from '@sveltejs/kit';
-
-export const actions = {
-	default: async ({ params }: RequestEvent) => {
-		const url = `${SECRET_BACK_REDIRECT_URL}/${params.provider}/redirect`;
-		redirect(302, url);
-	}
-};
