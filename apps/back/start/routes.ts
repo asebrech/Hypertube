965c881719ebb2bed8c9bd114879af2e26d4471b
asebrech <alois.sebrechts@gmail.com>
2025-06-05 16:58:40 +0200

    turbo repo

---
+++ apps/back/start/routes.ts
@@ -0,0 +1,54 @@
+/*
+|--------------------------------------------------------------------------
+| Routes file
+|--------------------------------------------------------------------------
+|
+| The routes file is used for defining the HTTP routes.
+|
+*/
+
+import router from '@adonisjs/core/services/router'
+import { middleware } from './kernel.js'
+
+const AuthController = () => import('#controllers/auth_controller')
+const MoviesController = () => import('#controllers/movies_controller')
+
+router.get('/', async () => {
+  return {
+    hello: 'world',
+  }
+})
+
+router
+  .group(() => {
+    router.post('register', [AuthController, 'register'])
+    router.post('login', [AuthController, 'login'])
+    router.post('logout', [AuthController, 'logout']).use(middleware.auth())
+  })
+  .prefix('user')
+
+router.group(() => {
+  router.get('movies', [MoviesController, 'index'])
+  router.get('movies/backdropImage', [MoviesController, 'backdropImage'])
+  router.get('movies/:id', [MoviesController, 'movieDetails'])
+})
+
+router
+  .get('me', async ({ auth, response }) => {
+    try {
+      const user = auth.getUserOrFail()
+      return response.ok(user)
+    } catch (error) {
+      return response.unauthorized({ error: 'User not found' })
+    }
+  })
+  .use(middleware.auth())
+
+router
+  .get('/:provider/redirect', ({ ally, params }) => {
+    const driverInstance = ally.use(params.provider)
+    return driverInstance.redirect()
+  })
+  .where('provider', /github|google/)
+
+router.get('/:provider/callback', [AuthController, 'callback']).where('provider', /github|google/)
---
+++ apps/front/src/routes/+layout.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+	import '../app.css';
+	import '$lib/i18n';
+	let { children } = $props();
+	import { ModeWatcher } from 'mode-watcher';
+	import { isLoading } from 'svelte-i18n';
+</script>
+
+<ModeWatcher defaultMode={'dark'} />
+{#if $isLoading}
+	Please wait...
+{:else}
+	{@render children?.()}
+{/if}
