c90480ef126e58a18847be2fd76a0be1801c6f6b
LaBoiteDeCarton <daniel.mercadiel@gmail.com>
2025-06-13 12:09:56 +0200

    details thumbnails updated

--- apps/front/src/lib/components/tadflix/movie-preview/movie-preview.svelte
+++ apps/front/src/lib/components/tadflix/movie-preview/movie-preview.svelte
@@ -4,6 +4,10 @@
 	import { getMovieDetails } from '@/services/api';
 	import type { MovieDetails } from '@hypertube/shared';
 	import { onMount } from 'svelte';
+	import ButtonPreview from '$lib/components/tadflix/buttons/button-preview/button-preview.svelte';
+	import { Play, Plus, ChevronDown } from 'lucide-svelte';
+	import Icon from '$lib/assets/tadflix-small.svelte';
+	import { Dot } from 'lucide-svelte';
 
 	export let movieId: number;
 	let isLoading = true;
@@ -26,36 +30,56 @@
 	});
 </script>
 
-<a href="/movie/{movieId}" target="_blank" rel="noreferrer noopener" class="block min-h-[380px]">
-	<div class="flex flex-col items-center">
+<a href="/movie/{movieId}" target="_blank" rel="noreferrer noopener" class="block">
+	<div class="bg-secondary flex flex-col items-center gap-2 pb-2">
 		{#if movie?.backdrop_path}
-			<img
-				src={`https://image.tmdb.org/t/p/w500${movie?.backdrop_path}`}
-				alt={movie?.title}
-				class="aspect-[5/3] w-[300px] rounded-[2px] object-cover"
-			/>
+			<div
+				class="flex aspect-[5/3] w-[300px] items-end rounded-[2px] bg-cover bg-center"
+				style="background-image: url('https://image.tmdb.org/t/p/w500{movie.backdrop_path}');"
+			>
+				<div class="w-full rounded-b-[2px] bg-gradient-to-t from-black/60 to-transparent p-4">
+					<Icon />
+					<h3 class="line-clamp-1 font-medium">{movie?.title}</h3>
+				</div>
+			</div>
 		{:else}
 			<div class="flex aspect-[5/3] w-[300px] items-center justify-center">
 				<Skeleton class="h-full w-full rounded-[2px]" />
 			</div>
 		{/if}
-		<div class="m-2 p-4">
-			<h3 class="line-clamp-1 font-medium">{movie?.title}</h3>
-			<p class="line-clamp-3 text-sm text-gray-500">{movie?.overview}</p>
+		<div class="flex w-full flex-col gap-2 px-4">
+			{#if isLoading}
+				<div class="mt-2">
+					<Skeleton class="h-6 w-24" />
+				</div>
+			{:else}
+				<div class="flex items-center justify-between">
+					<div class="flex gap-2">
+						<ButtonPreview variant="filled">
+							<Play fill={'black'} />
+						</ButtonPreview>
+						<ButtonPreview variant="outline">
+							<Plus />
+						</ButtonPreview>
+					</div>
+					<ButtonPreview variant="outline">
+						<ChevronDown />
+					</ButtonPreview>
+				</div>
+			{/if}
+			<!-- <p class="line-clamp-3 text-sm text-gray-500">{movie?.overview}</p> -->
 			{#if movie?.runtime}
 				<p class="text-sm text-gray-500">
 					{Math.floor(movie?.runtime / 60)}h {movie?.runtime % 60}m
 				</p>
 			{/if}
 			{#if movie?.genres.length > 0}
-				<div class="flex flex-wrap justify-center">
-					{#each movie?.genres as genre}
-						<Badge
-							class="m-1 rounded-[2px] bg-gray-200 px-2 py-1 text-sm text-gray-700"
-							key={genre.id}
-						>
-							{genre.name}
-						</Badge>
+				<div class="flex flex-wrap items-center">
+					{#each movie?.genres as genre, i (genre.id)}
+						<p class="font-montserrat text-[16px] font-normal">{genre.name}</p>
+						{#if i < movie.genres.length - 1}
+							<Dot color="gray" />
+						{/if}
 					{/each}
 				</div>
 			{/if}
