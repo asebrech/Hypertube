be9a9cf7fde707ecaae4bc64f9aa2a3ace5c5b18
LaBoiteDeCarton <daniel.mercadiel@gmail.com>
2025-06-19 18:50:37 +0200

    remove consol log

--- apps/back/app/services/tmdb_service.ts
+++ apps/back/app/services/tmdb_service.ts
@@ -132,7 +132,6 @@ export class TMDBService {
   async getPosterImageUrl(tmdb_movie_id: number, size: string, lang: string = 'en') {
     const endpoint = `/movie/${tmdb_movie_id}/images?language=${lang.split('-')[0]}`
     let langFound = true
-    console.log('TMDBService.getBackdropImageUrl', endpoint)
     let data = await this.getSomething(endpoint)
     let imageUrl = this.getPosterImageUrlFromData(data)
     if (!imageUrl) {
