fb7e8c6781b6e91189de5696d13a975988f76821
asebrech <alois.sebrechts@gmail.com>
2025-06-20 14:29:13 +0200

    can stream from back to front

--- apps/back/start/routes.ts
+++ apps/back/start/routes.ts
@@ -22,6 +22,8 @@ router.get('/', async () => {
 
 router.get('home', [TorrentController, 'torrent'])
 router.get('stream', [TorrentController, 'stream'])
+router.get('/hls/:videoId/*', [TorrentController, 'stream'])
+router.get('convert', [TorrentController, 'convert'])
 
 router
   .group(() => {
