549906d0478f7e125ac009e786afb939a858a7a8
LaBoiteDeCarton <daniel.mercadiel@gmail.com>
2025-06-27 10:22:28 +0200

    trying cookie to bearer middleware

--- apps/back/start/kernel.ts
+++ apps/back/start/kernel.ts
@@ -18,11 +18,10 @@ import server from '@adonisjs/core/services/server'
 server.errorHandler(() => import('#exceptions/handler'))
 
 /**
- * The server middleware stack runs middleware on all the HTTP
- * requests, even if there is no route registered for
- * the request URL.
+ * Custom middleware to extract token from cookie and inject it into Authorization header
  */
 server.use([
+  () => import('#middleware/token_cookie_to_auth_header'),
   () => import('#middleware/container_bindings_middleware'),
   () => import('#middleware/force_json_response_middleware'),
   () => import('@adonisjs/cors/cors_middleware'),
@@ -32,7 +31,10 @@ server.use([
  * The router middleware stack runs middleware on all the HTTP
  * requests with a registered route.
  */
-router.use([() => import('@adonisjs/core/bodyparser_middleware'), () => import('@adonisjs/auth/initialize_auth_middleware')])
+router.use([
+  () => import('@adonisjs/core/bodyparser_middleware'),
+  () => import('@adonisjs/auth/initialize_auth_middleware'),
+])
 
 /**
  * Named middleware collection must be explicitly assigned to
