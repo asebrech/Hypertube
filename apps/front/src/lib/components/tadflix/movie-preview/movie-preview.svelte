306ff166b04880d00a0fd2188be87ff6e73b46ca
LaBoiteDeCarton <daniel.mercadiel@gmail.com>
2025-07-11 21:36:41 +0200

    crrecting hover card content width to 150% and height position calculated

--- apps/front/src/lib/components/tadflix/movie-preview/movie-preview.svelte
+++ apps/front/src/lib/components/tadflix/movie-preview/movie-preview.svelte
@@ -167,7 +167,7 @@
 		</div>
 		{#if movie?.backdrop_path && (!playerReady || videoEnded)}
 			<div
-				class="flex aspect-[6/3] w-[300px] items-end rounded-[2px] bg-cover bg-center"
+				class="flex aspect-[6/3] w-full items-end rounded-[2px] bg-cover bg-center"
 				style="background-image: url('https://image.tmdb.org/t/p/w500{movie.backdrop_path}');"
 			>
 				<div class="z-20 w-full rounded-b-[2px] bg-gradient-to-t from-black/60 to-transparent p-4">
@@ -176,7 +176,7 @@
 				</div>
 			</div>
 		{:else if !movie?.backdrop_path! && (!playerReady || videoEnded)}
-			<div class="flex aspect-[5/3] w-[300px] items-center justify-center">
+			<div class="flex aspect-[5/3] w-full items-center justify-center">
 				<Skeleton class="h-full w-full rounded-[2px]" />
 			</div>
 		{/if}
