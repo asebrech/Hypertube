05a88e76d65beed085abbc8f5c80674c641e1675
LaBoiteDeCarton <daniel.mercadiel@gmail.com>
2025-08-08 19:05:58 +0200

    opensubtitles api calls added

--- apps/back/app/services/opensubtitle_service.ts
+++ apps/back/app/services/opensubtitle_service.ts
@@ -1,5 +1,6 @@
 import axios from "axios";
 import env from '#start/env'
+import { SubtitleApiResponse, SubtitleResult } from '@hypertube/shared'
 
 export class OpenSubtitleService {
   private apiKey: string | undefined;
@@ -10,7 +11,7 @@ export class OpenSubtitleService {
     this.apiUrl = env.get('OPENSUBTITLES_API_URL');
   }
 
-    private async getSomethingFromApi(endpoint: string) {
+  private async getSomethingFromApi(endpoint: string) {
     const url = `${this.apiUrl}${endpoint}`
     const options = {
       method: 'GET',
@@ -27,8 +28,46 @@ export class OpenSubtitleService {
     }
   }
 
+  private async postSomethingToApi(endpoint: string, data: any) {
+    const url = `${this.apiUrl}${endpoint}`
+    const options = {
+      method: 'POST',
+      url: url,
+      headers: {
+        'Api-Key': this.apiKey,
+        'Content-Type': 'application/json',
+      },
+      data: data,
+    }
+    try {
+      const response = await axios(options)
+      return response.data
+    } catch (error) {
+      throw new Error('Failed to post data to TMDB. Endpoint : ' + endpoint)
+    }
+  }
 
-  public async searchSubtitles(tmdb_id: string, lang: string = 'en'): Promise<any> {
+
+  public async searchSubtitles(tmdb_id: string, lang: string = 'en'): Promise<SubtitleResult> {
     const endpoint = `/subtitles?tmdb_id=${encodeURIComponent(tmdb_id)}&languages=${encodeURIComponent(lang)}`
+    const data: SubtitleApiResponse = await this.getSomethingFromApi(endpoint) as SubtitleApiResponse;
+    const subtitle = await data.data.find((item: any) => item.attributes.language === lang && item.attributes.tmdb_id == tmdb_id);
+    return subtitle;
+  }
+
+  public async downloadSubtitle(file_id: string): Promise<string> {
+    const endpoint = `/download`
+    const data = {
+      file_id: file_id,
+    };
+    const data: { link: string } = await this.postSomethingToApi(endpoint, data);
+    return data.link;
+  }
+
+  public async getSubtitleLink(tmdb_id: string, lang: string = 'en'): Promise<string | null> {
+    const subtitle = await this.searchSubtitles(tmdb_id, lang);
+    if (subtitle) {
+      return await this.downloadSubtitle(subtitle.attributes.files[0].file_id.toString());
+    }
   }
 }
--- packages/shared/src/types.ts
+++ packages/shared/src/types.ts
@@ -166,3 +166,64 @@ export type PersonDetails = {
 	popularity: number;
 	profile_path: string;
 };
+
+export type SubtitleApiResponse = {
+  total_pages: number,
+  total_count: number,
+  per_page: number,
+  page: number,
+  data: SubtitleResult[]
+}
+
+export type SubtitleResult = {
+    id: string;
+    type: "subtitle";
+    attributes: {
+      subtitle_id: string;
+      language: string;
+      download_count: number;
+      new_download_count: number;
+      hearing_impaired: boolean;
+      hd: boolean;
+      fps: number;
+      votes: number;
+      ratings: number;
+      from_trusted: boolean;
+      foreign_parts_only: boolean;
+      upload_date: string;
+      ai_translated: boolean;
+      nb_cd: number;
+      slug: string;
+      machine_translated: boolean;
+      release: string;
+      comments: string;
+      legacy_subtitle_id: number;
+      legacy_uploader_id: number;
+      uploader: {
+        uploader_id: number | null;
+        name: string;
+        rank: string;
+      };
+      feature_details: {
+        feature_id: number;
+        feature_type: string;
+        year: number;
+        title: string;
+        movie_name: string;
+        imdb_id: number;
+        tmdb_id: number;
+      };
+      url: string;
+      related_links: {
+        label: string;
+        url: string;
+        img_url: string;
+      }[];
+      files: {
+        file_id: number;
+        cd_number: number;
+        file_name: string;
+      };
+    };
+  };
+
