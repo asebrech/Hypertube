e96f5481cf14cc0b5ce9c27871a32617c3e29019
Thomas <thomas@livmeds.com>
2025-08-29 14:56:27 +0200

    Add loading state handling in layout component

--- apps/front/src/routes/(auth)/(authed)/+layout.svelte
+++ apps/front/src/routes/(auth)/(authed)/+layout.svelte
@@ -1,10 +1,26 @@
 <script lang="ts">
+	import { isLoading } from 'svelte-i18n';
+	import { Loader2 } from 'lucide-svelte';
+	import { i18nReady } from '$lib/i18n';
+	
 	let { children } = $props();
+	
+	let ready = $state(false);
+
+	i18nReady?.then(() => {
+		ready = true;
+	});
 </script>
 
 <div class="min-h-screen bg-cover bg-center bg-no-repeat relative" style="background-image: url('/img/netflix-background.jpg');">
 	<div class="absolute inset-0 bg-black/50"></div>
 	<div class="relative z-10 flex flex-1 items-center justify-center min-h-screen">
-		{@render children()}
+		{#if $isLoading || !ready}
+			<div class="flex items-center justify-center">
+				<Loader2 class="size-8 animate-spin text-white" />
+			</div>
+		{:else}
+			{@render children()}
+		{/if}
 	</div>
 </div>
