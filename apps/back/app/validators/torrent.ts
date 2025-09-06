376d0fae907d5c8f8b600057884034d4aef358d1
asebrech <alois.sebrechts@gmail.com>
2025-09-06 19:07:01 +0200

    fix validator

--- apps/back/app/validators/torrent.ts
+++ apps/back/app/validators/torrent.ts
@@ -1,21 +1,24 @@
 import vine from '@vinejs/vine'
 import { isValidTmdbId } from '../utils/format.js'
 
+const tmdbIdRule = vine.createRule((value, options, field) => {
+  const parsed = Number.parseInt(value)
+  if (Number.isNaN(parsed)) {
+    field.report('Invalid movie ID provided', 'tmdb_id', field)
+    return
+  }
+  if (!isValidTmdbId(parsed)) {
+    field.report('Invalid movie ID provided', 'tmdb_id', field)
+  }
+})
+
 export const tmdbIdValidator = vine.compile(
   vine.object({
     id: vine
       .string()
+      .use(tmdbIdRule())
       .transform((value) => {
-        const parsed = Number.parseInt(value)
-        if (Number.isNaN(parsed)) {
-          throw new Error('Invalid movie ID provided')
-        }
-        return parsed
-      })
-      .use((value, field, report) => {
-        if (!isValidTmdbId(value)) {
-          report(field, 'Invalid movie ID provided', 'tmdb_id', {})
-        }
+        return Number.parseInt(value)
       }),
   })
 )
